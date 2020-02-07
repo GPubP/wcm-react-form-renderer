07c08aa064bc1fa81de6e52206de59a172aa3eff
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-02-07 11:20:30 +0100

    chore: add husky conf

--- public/index.tsx
+++ public/index.tsx
@@ -1,6 +1,6 @@
-
-
 import RedactionForm from './lib/components/Form/Form';
+export * from './lib/core.types'
+export * from './lib/services/fieldRegistry/fieldRegistry.types';
 
 export {
 	RedactionForm,
--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,51 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-	mode: 'production',
-	devtool: 'source-map',
-	entry: './public/index.tsx',
-	module: {
-		rules: [
-			{
-				test: /\.ts(x)?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
+module.exports = env => {
+	const defaultConfig = {
+		mode: 'production',
+		devtool: 'source-map',
+		entry: './public/index.tsx',
+		performance: {
+			hints: false,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts(x)?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: [ '.tsx', '.ts', '.js' ],
+		},
+		plugins: [
+			// clean dist folder before every build
+			new CleanWebpackPlugin(),
 		],
-	},
-	resolve: {
-		extensions: [ '.tsx', '.ts', '.js' ],
-	},
-	plugins: [
-		// clean dist folder before every build
-		new CleanWebpackPlugin(),
-		new BundleAnalyzerPlugin(),
-	],
-	externals: {
-		'react': 'react',
-		'react-dom': 'react-dom',
-		'rc-slider': 'rc-slider',
-	},
-	output: {
-		filename: 'redactie-form-renderer.umd.js',
-		path: path.resolve(__dirname, 'dist'),
-		libraryTarget: 'umd',
-	},
+		externals: {
+			'react': 'react',
+			'react-dom': 'react-dom',
+			'rc-slider': 'rc-slider',
+		},
+		output: {
+			filename: 'redactie-form-renderer.umd.js',
+			path: path.resolve(__dirname, 'dist'),
+			libraryTarget: 'umd',
+		},
+	};
+
+	if (env.analyse) {
+		return {
+			...defaultConfig,
+			plugins: [
+				...defaultConfig.plugins,
+				new BundleAnalyzerPlugin(),
+			],
+		}
+	}
+
+	return defaultConfig;
 }
