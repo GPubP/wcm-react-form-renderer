19b820e1bfa92e4f2deb1cc9bb3dc9fcec75cd01
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-02-21 12:56:29 +0100

    feat(sites-demo): register module on /sites for demo

--- public/index.tsx
+++ public/index.tsx
@@ -1,19 +1,15 @@
-//import React, { FC } from 'react';
 import Core from '@redactie/redactie-core';
+import React, { FC } from 'react';
 
 import RedactionForm from './lib/components/Form/Form';
-//import { FormSchema } from './lib/core.types';
+import { FormSchema } from './lib/core.types';
 import { fieldRegistry } from './lib/services';
 
 /**
- * FormsRouteComponent can be used to test the FormsComponent in the Redactie POC
- * Steps to make it work:
- * 	- prepare the ckeditor module for testing in poc
- * 	- uncomment following code and imports
- * 	- add " && cp dist/redactie-form-renderer.umd.js ../core-app/public/redactie-form-renderer.js" to the build script in package.json
+ * for now, FormsRouteComponent is used as Demo code.
+ */
 
 const FormsRouteComponent: FC = () => {
-
 	const form: FormSchema = {
 		fields: [
 			{
@@ -34,32 +30,37 @@ const FormsRouteComponent: FC = () => {
 				},
 			},
 		],
-	}
+	};
 
 	return (
 		<>
 			<h1>Forms Module Route</h1>
-			<RedactionForm onSubmit={(value) => { console.log('submit', value) }} schema={form} />
+			<RedactionForm
+				onSubmit={value => {
+					console.log('submit', value);
+				}}
+				schema={form}
+			/>
 		</>
-	)
+	);
 };
-
-// register formsrenderer route
-Core.routes.register({
-	path: '/formsrenderer',
-	component: FormsRouteComponent,
-	label: 'Forms Renderer',
-});
-
-*
-*
-*/
 
 // expose module
 Core.modules.exposeModuleApi('forms-module', {
 	form: RedactionForm,
 	fieldRegistry: fieldRegistry,
 });
+
+const sitesModule = Core.modules.getModuleAPI('sites-module');
+
+// expose route on /sites
+if (sitesModule) {
+	sitesModule.routes.register({
+		component: FormsRouteComponent,
+		label: 'form',
+		path: '/form',
+	});
+}
 
 export * from './lib/core.types';
 export * from './lib/services/fieldRegistry/fieldRegistry.types';
