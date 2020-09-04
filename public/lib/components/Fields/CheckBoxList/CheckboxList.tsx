efe10f8147eb845097f973d27283574b880a330c
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-09-04 08:55:11 +0200

    feat: hide field when is has only one allowed option

--- public/lib/components/Fields/CheckBoxList/CheckboxList.tsx
+++ public/lib/components/Fields/CheckBoxList/CheckboxList.tsx
@@ -1,62 +1,82 @@
 import { Checkbox } from '@acpaas-ui/react-components';
 import classNames from 'classnames';
 import { FieldArray } from 'formik';
-import React, { ChangeEvent, FC, useMemo } from 'react';
+import React, { ChangeEvent, FC, useEffect, useMemo } from 'react';
 
 import { InputFieldProps } from '../../../services/fieldRegistry';
 import { filterAllowedOptions } from '../../../utils';
 import { ErrorMessage } from '../../ErrorMessage';
 
-const CheckboxList: FC<InputFieldProps> = ({ fieldProps, fieldSchema }) => {
+const CheckboxList: FC<InputFieldProps> = ({ fieldProps, fieldSchema, fieldHelperProps }) => {
 	const {
 		name,
 		label,
 		config = {
 			required: false,
 			options: [],
-			allowedOptions: [],
+			hideWhenOnlyOneAllowedOption: false,
 		},
 	} = fieldSchema;
 	const { field } = fieldProps;
 	const labelClass = classNames('a-input', {
 		'is-required': config.required,
 	});
+	const showField = !(
+		config.hideWhenOnlyOneAllowedOption &&
+		Array.isArray(config.allowedOptions) &&
+		config.allowedOptions.length === 1
+	);
+
+	/**
+	 * Hooks
+	 */
 	const options = useMemo(() => filterAllowedOptions(config.options, config.allowedOptions), [
 		config.options,
 		config.allowedOptions,
 	]);
 
+	useEffect(() => {
+		// Automatically select the first allowed option when the select field is hidden from the user
+		if (!showField && field.value !== config.allowedOptions[0]) {
+			fieldHelperProps.setValue(config.allowedOptions[0]);
+		}
+	}, [config.allowedOptions, field.value, fieldHelperProps, showField]);
+
 	return (
-		<div>
-			<div className={labelClass}>
-				<label className="a-input__label">{label}</label>
-			</div>
-			<FieldArray
-				name={name}
-				render={arrayHelpers => (
-					<div>
-						{options.map(option => (
-							<Checkbox
-								key={`${name}-${option.value}`}
-								id={`${name}-${option.value}`}
-								label={option.label}
-								disabled={!!config.disabled}
-								checked={!!field.value?.includes(option.value)}
-								onChange={(e: ChangeEvent<HTMLInputElement>) => {
-									if (e.target.checked) {
-										arrayHelpers.push(option.value);
-									} else {
-										const idx = field.value.indexOf(option.value);
-										arrayHelpers.remove(idx);
-									}
-								}}
-							/>
-						))}
+		<>
+			{showField && (
+				<div>
+					<div className={labelClass}>
+						<label className="a-input__label">{label}</label>
 					</div>
-				)}
-			/>
-			<ErrorMessage name={field.name} />
-		</div>
+					<FieldArray
+						name={name}
+						render={arrayHelpers => (
+							<div>
+								{options.map(option => (
+									<Checkbox
+										key={`${name}-${option.value}`}
+										id={`${name}-${option.value}`}
+										label={option.label}
+										disabled={!!config.disabled}
+										checked={!!field.value?.includes(option.value)}
+										onChange={(e: ChangeEvent<HTMLInputElement>) => {
+											if (e.target.checked) {
+												arrayHelpers.push(option.value);
+											} else {
+												const idx = field.value.indexOf(option.value);
+												arrayHelpers.remove(idx);
+											}
+										}}
+									/>
+								))}
+							</div>
+						)}
+					/>
+					<ErrorMessage name={field.name} />
+				</div>
+			)}
+		</>
 	);
 };
 
--- public/lib/components/Fields/Radio/Radio.tsx
+++ public/lib/components/Fields/Radio/Radio.tsx
@@ -1,36 +1,60 @@
 import { RadioGroup } from '@acpaas-ui/react-components/packages/form';
 import { omit } from 'ramda';
-import React, { FC, useMemo } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 
 import { InputFieldProps } from '../../../services/fieldRegistry';
 import { filterAllowedOptions } from '../../../utils';
 import { ErrorMessage } from '../../ErrorMessage';
 
-const InputRadio: FC<InputFieldProps> = ({ fieldProps, fieldSchema }: InputFieldProps) => {
+const InputRadio: FC<InputFieldProps> = ({
+	fieldProps,
+	fieldSchema,
+	fieldHelperProps,
+}: InputFieldProps) => {
 	const {
 		name,
 		label,
 		config = {
 			options: [],
-			allowedOptions: [],
+			hideWhenOnlyOneAllowedOption: false,
 		},
 	} = fieldSchema;
 	const { field } = fieldProps;
+	const showField = !(
+		config.hideWhenOnlyOneAllowedOption &&
+		Array.isArray(config.allowedOptions) &&
+		config.allowedOptions.length === 1
+	);
+
+	/**
+	 * Hooks
+	 */
 	const options = useMemo(() => filterAllowedOptions(config.options, config.allowedOptions), [
 		config.options,
 		config.allowedOptions,
 	]);
 
+	useEffect(() => {
+		// Automatically select the first allowed option when the select field is hidden from the user
+		if (!showField && field.value !== config.allowedOptions[0]) {
+			fieldHelperProps.setValue(config.allowedOptions[0]);
+		}
+	}, [config.allowedOptions, field.value, fieldHelperProps, showField]);
+
 	return (
 		<>
-			<RadioGroup
-				id={name}
-				label={label}
-				options={options}
-				{...omit(['multiLanguage', 'min', 'max', 'options'])(config)}
-				{...field}
-			/>
-			<ErrorMessage name={field.name} />
+			{showField && (
+				<>
+					<RadioGroup
+						id={name}
+						label={label}
+						options={options}
+						{...omit(['multiLanguage', 'min', 'max', 'options'])(config)}
+						{...field}
+					/>
+					<ErrorMessage name={field.name} />
+				</>
+			)}
 		</>
 	);
 };
