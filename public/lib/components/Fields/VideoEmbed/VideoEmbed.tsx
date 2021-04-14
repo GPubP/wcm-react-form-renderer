d1fbb28f875bcd9bbbfd1e59e326a073c7924c59
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-04-14 15:35:16 +0200

    fix(VideoEmbed): field and view iframe init

--- public/lib/components/Fields/VideoEmbed/VideoEmbed.tsx
+++ public/lib/components/Fields/VideoEmbed/VideoEmbed.tsx
@@ -1,22 +1,21 @@
 import { TextField } from '@acpaas-ui/react-components';
 import classnames from 'classnames/bind';
 import { pick } from 'ramda';
-import React, { ChangeEvent, FC, useMemo, useState } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { InputFieldProps } from '../../../services/fieldRegistry';
 import { ErrorMessage } from '../../ErrorMessage';
 import { VideoIFrame } from '../../VideoIFrame';
 import { DEFAULT_FIELD_CONFIG_PROPS } from '../Fields.const';
 
-import { VIDEO_EMBED_PROVIDERS } from './VideoEmbed.const';
+import { getProviderUrl } from './VideoEmbed.helpers';
 import styles from './VideoEmbed.module.scss';
 
 const cx = classnames.bind(styles);
 
-const VideoEmbed: FC<InputFieldProps> = ({ fieldSchema, fieldProps, fieldHelperProps }) => {
+const VideoEmbed: FC<InputFieldProps> = ({ fieldSchema, fieldProps }) => {
 	const { config = {}, name, label } = fieldSchema;
 	const { field, meta } = fieldProps;
-	const { setValue } = fieldHelperProps;
 
 	const state = !!meta.error && !!meta.touched ? 'error' : '';
 
@@ -24,75 +23,14 @@ const VideoEmbed: FC<InputFieldProps> = ({ fieldSchema, fieldProps, fieldHelperP
 	 * Hooks
 	 */
 
-	const [iframeSrc, setIframeSrc] = useState<string>('');
+	const iframeSrc = useMemo(() => {
+		if (!field.value || typeof field.value !== 'string') {
+			return null;
+		}
+		return getProviderUrl(field.value);
+	}, [field.value]);
 	// Pick only the known properties from the config object
 	const fieldConfigProps = useMemo(() => pick(DEFAULT_FIELD_CONFIG_PROPS, config), [config]);
-
-	/**
-	 * Methods
-	 */
-
-	const getUrlMatches = (url: string, pattern: RegExp): string[] | null => {
-		// 1. Try to match without stripping the protocol and "www" subdomain.
-		let match = url.match(pattern);
-
-		if (match) {
-			return match;
-		}
-
-		// 2. Try to match after stripping the protocol.
-		let rawUrl = url.replace(/^https?:\/\//, '');
-		match = rawUrl.match(pattern);
-
-		if (match) {
-			return match;
-		}
-
-		// 3. Try to match after stripping the "www" subdomain.
-		rawUrl = rawUrl.replace(/^www\./, '');
-		match = rawUrl.match(pattern);
-
-		if (match) {
-			return match;
-		}
-
-		return null;
-	};
-
-	const getProviderUrl = (url: string): string | undefined => {
-		const trimmedUrl = url.trim();
-
-		for (const provider of VIDEO_EMBED_PROVIDERS) {
-			for (const regex of provider.pattern) {
-				const match = getUrlMatches(trimmedUrl, regex);
-
-				if (match) {
-					return provider.url(match[1]);
-				}
-			}
-		}
-	};
-
-	const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
-		if (!e.target.value) {
-			// Clear iframe src when the input gets manually cleared
-			if (iframeSrc) {
-				setIframeSrc('');
-			}
-			setValue('');
-			return;
-		}
-
-		const providerUrl = getProviderUrl(e.target.value);
-
-		if (providerUrl) {
-			setIframeSrc(providerUrl);
-		} else {
-			setIframeSrc('');
-		}
-
-		setValue(e.target.value);
-	};
 
 	/**
 	 * Render
@@ -108,7 +46,6 @@ const VideoEmbed: FC<InputFieldProps> = ({ fieldSchema, fieldProps, fieldHelperP
 					label={label}
 					{...field}
 					{...fieldConfigProps}
-					onChange={onChange}
 				/>
 				<ErrorMessage name={field.name} />
 			</div>
--- public/lib/components/Views/VideoEmbed/VideoEmbed.tsx
+++ public/lib/components/Views/VideoEmbed/VideoEmbed.tsx
@@ -1,14 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { ViewFieldProps } from '../../../services/viewRegistry/viewRegistry.types';
+import { getProviderUrl } from '../../Fields/VideoEmbed/VideoEmbed.helpers';
 import { VideoIFrame } from '../../VideoIFrame';
 
 const VideoEmbedView: FC<ViewFieldProps> = ({ value }) => {
-	if (!value || typeof value !== 'string') {
+	const iframeUrl = useMemo(() => {
+		if (!value || typeof value !== 'string') {
+			return null;
+		}
+		return getProviderUrl(value);
+	}, [value]);
+
+	if (!iframeUrl) {
 		return null;
 	}
 
-	return <VideoIFrame src={value} />;
+	return <VideoIFrame src={iframeUrl} />;
 };
 
 export default VideoEmbedView;
