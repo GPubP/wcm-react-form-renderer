bd546acbbc7940fd90a1bc0db3bcb88105569c13
Andry Charlier <andrycharlier@gmail.com>
2021-06-07 17:08:23 +0200

    fix(TimePeriods): replace modal and don't show it in repeater context

--- public/lib/components/Fields/TimePeriods/TimePeriods.tsx
+++ public/lib/components/Fields/TimePeriods/TimePeriods.tsx
@@ -1,24 +1,15 @@
 import { Button } from '@acpaas-ui/react-components';
-import {
-	ActionBar,
-	ActionBarContentSection,
-	ControlledModal,
-} from '@acpaas-ui/react-editorial-components';
-import classnames from 'classnames/bind';
 import { FormikProps } from 'formik';
 import { isNil } from 'ramda';
-import React, { FC, ReactElement, useState } from 'react';
+import React, { FC, useState } from 'react';
 
-import { CORE_TRANSLATIONS, useCoreTranslation } from '../../../connectors';
 import { InputFieldProps } from '../../../services/fieldRegistry';
 
-import { CreateTimePeriodsForm, CreateTimePeriodsFormState } from './CreateTimePeriodsForm';
+import { CreateTimePeriodsFormState } from './CreateTimePeriodsForm';
 import { INITIAL_CREATE_FORM_STATE } from './CreateTimePeriodsForm/CreateTimePeriodsForm.const';
+import { CreateTimePeriodsModal } from './CreateTimePeriodsModal';
 import { EditTimePeriodsForm } from './EditTimePeriodsForm';
-import styles from './TimePeriods.module.scss';
 import { TimePeriodsFormState, TimePeriodsValue } from './TimePeriods.types';
-
-const cx = classnames.bind(styles);
 
 const TimePeriods: FC<InputFieldProps> = ({ fieldProps, fieldHelperProps, fieldSchema }) => {
 	const { config } = fieldSchema;
@@ -33,9 +24,8 @@ const TimePeriods: FC<InputFieldProps> = ({ fieldProps, fieldHelperProps, fieldS
 	 * Hooks
 	 */
 
-	// Show modal by default in repeater context
+	// Don't show modal in repeater context
 	const [showModal, setShowModal] = useState(isRepeated ?? false);
-	const [t] = useCoreTranslation();
 
 	/**
 	 * Methods
@@ -60,63 +50,30 @@ const TimePeriods: FC<InputFieldProps> = ({ fieldProps, fieldHelperProps, fieldS
 	 * Render
 	 */
 
-	const renderAddButton = (): ReactElement | null => {
-		// Don't show add button in repeater context, the modal will open by default
-		return !isRepeated ? (
-			<Button
-				onClick={() => setShowModal(true)}
-				iconLeft="plus"
-				size="small"
-				type="transparent"
-				htmlType="button"
-			>
-				Voeg tijdstip toe
-			</Button>
-		) : null;
-	};
+	if (!hasFieldValue && isRepeated) {
+		return null;
+	}
 
 	return !hasFieldValue ? (
-		<>
-			{renderAddButton()}
-			<ControlledModal
-				className={cx('o-time-periods__modal')}
-				overlayClassName={cx('o-time-periods__overlay')}
-				show={showModal}
-				size="large"
-			>
-				<div className="u-padding">
-					<h3>Tijdstippen toevoegen</h3>
-				</div>
-				<div className="u-bg-light">
-					<CreateTimePeriodsForm onSubmit={onSetFieldValue}>
-						{({ resetForm, submitForm }) => {
-							return (
-								<ActionBar
-									className={cx('o-time-periods__modal-actions')}
-									disablePortal
-									isOpen
-								>
-									<ActionBarContentSection>
-										<div className="u-text-right">
-											<Button
-												className="u-margin-right"
-												negative
-												onClick={() => onCancel(resetForm)}
-											>
-												{t(CORE_TRANSLATIONS.BUTTON_CANCEL)}
-											</Button>
-											<Button onClick={submitForm}>
-												{t(CORE_TRANSLATIONS.BUTTON_ADD)}
-											</Button>
-										</div>
-									</ActionBarContentSection>
-								</ActionBar>
-							);
-						}}
-					</CreateTimePeriodsForm>
-				</div>
-			</ControlledModal>
-		</>
+		// When repeated the parent will handle the adding of time periods
+		!isRepeated ? (
+			<>
+				<Button
+					onClick={() => setShowModal(true)}
+					iconLeft="plus"
+					size="small"
+					type="transparent"
+					htmlType="button"
+				>
+					Voeg tijdstip toe
+				</Button>
+				<CreateTimePeriodsModal
+					show={showModal}
+					onCancel={onCancel}
+					onSubmit={onSetFieldValue}
+				/>
+			</>
+		) : null
 	) : (
 		<EditTimePeriodsForm
 			initialState={fieldValue as TimePeriodsFormState}
