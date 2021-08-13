f23381e957965c7e311e0e5a3881861cdc12f897
smtsj <jo.smets@studiohyperdrive.be>
2021-08-13 14:32:12 +0200

    fix: regex

--- public/lib/components/Fields/Datepicker/Datepicker.tsx
+++ public/lib/components/Fields/Datepicker/Datepicker.tsx
@@ -46,7 +46,9 @@ const Datepicker: React.FC<InputFieldProps> = ({ fieldProps, fieldSchema }: Inpu
 		}
 
 		if (
-			new RegExp('^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]d{4}$').test(field.value)
+			new RegExp('^(0[1-9]|1[0-9]|2[0-9]|3[0-1])/(0[1-9]|1[0-2])/20[0-9][0-9]$').test(
+				field.value
+			)
 		) {
 			return field.value;
 		}
