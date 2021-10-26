8a534567fee968dba42dc373efeda28ae48afcd6
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2021-10-26 21:44:50 +0200

    fix: funky date behaviour (removal and manual mode)

---
+++ public/lib/components/Fields/Datepicker/Datepicker.const.ts
@@ -0,0 +1,14 @@
+export const isAUCompleteDateRegex = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+
+export const ALLOWED_DATEPICKER_PROPS = [
+	'description',
+	'locale',
+	'selectedDates',
+	'open',
+	'autoClose',
+	'readOnly',
+	'noWeekends',
+	'minDate',
+	'maxDate',
+	'size',
+];
