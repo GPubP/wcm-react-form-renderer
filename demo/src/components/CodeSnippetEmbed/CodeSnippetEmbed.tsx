1b9e92fc2c5ed49f1b190e55a3293cfad5dce53b
Sander Geenen <sander.geenen@studiohyperdrive.be>
2021-10-29 10:29:14 +0200

    chore(code-snippet-embed): check if url is provided

--- demo/src/components/CodeSnippetEmbed/CodeSnippetEmbed.tsx
+++ demo/src/components/CodeSnippetEmbed/CodeSnippetEmbed.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { ViewFieldProps } from '../../../services/viewRegistry/viewRegistry.types';
+import { ViewFieldProps } from '@redactie/form-renderer-module';
 
 const CodeSnippetEmbedView: FC<ViewFieldProps> = ({ value }) => {
 	if (!value || !value?.url || typeof value?.url !== 'string') {
@@ -9,6 +9,7 @@ const CodeSnippetEmbedView: FC<ViewFieldProps> = ({ value }) => {
 
 	return (
 		<iframe
+			title="Titel van iframe"
 			style={{
 				width: value.width || '100%',
 				height: value.height || 300,
