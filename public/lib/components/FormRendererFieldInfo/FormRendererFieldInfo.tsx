492975484a34f594480d023d3b21d42b4cbeccf3
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2022-01-04 15:33:30 +0100

    feat(red-1398): added alertdialog padding

--- public/lib/components/FormRendererFieldInfo/FormRendererFieldInfo.tsx
+++ public/lib/components/FormRendererFieldInfo/FormRendererFieldInfo.tsx
@@ -5,7 +5,7 @@ export const FormRendererFieldInfo: FC<FormRendererFieldInfoProps> = ({ content 
 	const [visible, setVisibility] = useState(true);
 
 	return visible ? (
-		<div role="alertdialog" aria-labelledby="alert" className="m-alert">
+		<div role="alertdialog" aria-labelledby="alert" className="m-alert u-margin-top-xs">
 			<button className="a-button-transparent has-icon m-alert__close" aria-label="Close" onClick={() => setVisibility(false)}>
 				<span className="fa fa-close" aria-hidden="true"></span>
 			</button>
