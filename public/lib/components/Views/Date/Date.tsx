2b8b5ecef57002cb69965a6ae0cce0a7ad5dd0aa
Daren Malfait <github@daren.be>
2022-01-25 12:23:53 +0100

    fix(views-date): add multiple formats based on input

--- public/lib/components/Views/Date/Date.tsx
+++ public/lib/components/Views/Date/Date.tsx
@@ -16,12 +16,23 @@ const DateView: FC<ViewFieldProps> = ({ fieldSchema, value }) => {
 		return null;
 	}
 
-	// incoming date format is YYYY-MM-DD
-	const formatDate = moment(value, 'YYYY-MM-DD').format(
-		DATE_FORMATS[config.dateFormat] || DATE_FORMATS.dayMonthAndYear
-	);
+	let formattedDate = value;
 
-	return <>{formatDate}</>;
+	// incoming date format is YYYY-MM-DD
+	if (new RegExp('^\\d{4}[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$').test(value)) {
+		formattedDate = moment(value, 'YYYY-MM-DD').format(
+			DATE_FORMATS[config.dateFormat] || DATE_FORMATS.dayMonthAndYear
+		);
+	}
+
+	// incoming date format is DD/MM/YYYY
+	if (new RegExp('^(0[1-9]|1[0-9]|2[0-9]|3[0-1])\\/(0[1-9]|1[0-2])\\/\\d{4}$').test(value)) {
+		formattedDate = moment(value, 'DD/MM/YYYY').format(
+			DATE_FORMATS[config.dateFormat] || DATE_FORMATS.dayMonthAndYear
+		);
+	}
+
+	return <>{formattedDate}</>;
 };
 
 export default DateView;
