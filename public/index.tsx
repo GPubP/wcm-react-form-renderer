75f50dff7173a97f0f928e2e92e5ba88e8b2b02a
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2022-03-28 15:18:38 +0200

    feat: add activate language

--- public/index.tsx
+++ public/index.tsx
@@ -48,7 +48,7 @@ export * from './lib/core.types';
 export * from './lib/services/fieldRegistry/fieldRegistry.types';
 export * from './lib/services/viewRegistry/viewRegistry.types';
 export * from './lib/context/FieldRendererContext/FieldRendererContext.types';
-export * from './lib/context/FormContext/FormContext.types';
+export * from './lib/context/FormContext';
 export * from './lib/helpers/parseFields/parseFields.types';
 export * from './lib/helpers/getValueSyncMap/getValueSyncMap.types';
 export { default as Form } from './lib/components/Form/Form';
